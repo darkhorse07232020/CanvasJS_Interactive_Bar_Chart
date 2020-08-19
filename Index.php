f753655b2fbfe1cb4bc02831580a96561b10177d
Darkhorse07232020 <darkhorse0723@outlook.com>
2020-08-20 07:53:26 +0800

    Mid Commit

--- Index.php
+++ Index.php
@@ -5,14 +5,18 @@
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <title>Patient table</title>
-  <link href="/assets/css/style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="/assets/js/d3.js"></script>
   <style>
     rect {
-      fill: steelblue;
+      fill: darkblue;
       fill-opacity: 0.8;
     }
-
+    rect1 {
+      fill: grey;
+      fill-opacity: 0.8;
+    }
     rect:hover {
       fill-opacity: 1;
     }
@@ -20,67 +24,85 @@
     .axis {
       font-size: smaller;
     }
+
+    svg text.label {
+      fill: white;
+      font: 15px;
+      font-weight: 400;
+      text-anchor: middle;
+    }
   </style>
 </head>
 
 <body>
-  <h1>Student's First Barchart</h1>
+  <div class='container mt-3 border' align='center' id='main_area'>
+    <h1>Patient Chart</h1>
+    <div id='chart_area'></div>
 
-  <!--html filter checkbox-->
-  <div>
-    <strong>Filter:</strong>
-    <label><input type="checkbox" name="US" value="1" id="filter-us-only" />US only</label>
+    <div class='row' style="text-align: left;">
+      <div id='filter_area0' class="form-check col-4"></div>
+      <div id='filter_area1' class="form-check col-4"></div>
+      <div id='filter_area2' class="form-check col-4"></div>
+    </div>
   </div>
-  
+
   <script type="text/javascript">
-    const margin = { top: 40, bottom: 10, left: 120, right: 20 };
-    const width = 800 - margin.left - margin.right;
-    const height = 600 - margin.top - margin.bottom;
 
-    // Creates sources <svg> element
-    const svg = d3
-      .select("body")
+    mxwidth=$('#main_area').width();
+
+    const margin = { top: 40, bottom: 10, left: 250, right: 20 };
+    
+    var widthX = mxwidth - margin.left - margin.right,
+      heightX = 400 - margin.top - margin.bottom,
+      delim = 4;
+
+    var svgX = d3.select('#chart_area')
       .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom);
+      .attr("width", widthX + margin.left + margin.right)
+      .attr("height", heightX + margin.top + margin.bottom);
 
-    // Group used to enforce margin
-    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+    var g = svgX.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
     // Global variable for all data
-    let data;
+    var data;
 
     // Scales setup
-    const xscale = d3.scaleLinear().range([0, width]);
-    const yscale = d3.scaleBand().rangeRound([0, height]).paddingInner(0.1);
+    const scaleX = d3.scaleLinear().range([0, widthX]);
+    const scaleY = d3.scaleBand().rangeRound([0, heightX]).padding(0.2);
 
     // Axis setup
-    const xaxis = d3.axisTop().scale(xscale);
+    const xaxis = d3.axisTop().scale(scaleX);
     const g_xaxis = g.append("g").attr("class", "x axis");
-    const yaxis = d3.axisLeft().scale(yscale);
+    const yaxis = d3.axisLeft().scale(scaleY);
     const g_yaxis = g.append("g").attr("class", "y axis");
 
-    /////////////////////////
-    // TODO use the provided code sample (see html checkbox and change handler)
-    // when checked just the entries where `location.country === 'US'` should be shown
-
     d3.json("/data/data.json").then((json) => {
-      data = json;
-      console.log(data);
+      data = json.map(el => {
+        return {
+          "risk group": el['risk group'],
+          'value': Math.round(el['vaccinated']*100/el['registered'])
+      }});
+      update_filter(data);
       update(data);
     });
-
+    
     function update(new_data) {
+      var new_data=new_data.filter(function(d){
+        if(document.getElementById(d['risk group']).checked){
+          return true;
+        } else {
+          return false;
+        }
+      });
+      // console.log(new_data);
       //update the scales
-      xscale.domain([0, 100]);
-      yscale.domain(new_data.map((d) => d['risk group']));
+      scaleX.domain([0, 100]);
+      scaleY.domain(new_data.map((d) => d['risk group']));
+
       //render the axis
       g_xaxis.call(xaxis);
       g_yaxis.call(yaxis);
 
-      // Render the chart with new data
-
-      // DATA JOIN
       const rect = g
         .selectAll("rect")
         .data(new_data)
@@ -99,22 +121,31 @@
           // elements that aren't associated with data
           (exit) => exit.remove()
         );
-
-      // ENTER + UPDATE
-      // both old and new elements
-      rect
-        .attr("height", yscale.bandwidth())
-        .attr("width", (d) => xscale(d['vaccinated']*100/d['registered']))
-        .attr("y", (d) => yscale(d['risk group']));
       
+      rect
+        .attr("height", scaleY.bandwidth())
+        .attr("width", (d) => scaleX(Math.round(d.value)))
+        .attr("y", (d) => scaleY(d['risk group']));
+
       rect.select("title").text((d) => d['risk group']);
     }
 
-    //interactivity
-    d3.select("#filter-us-only").on("change", function () {
-      // This will be triggered when the user selects or unselects the checkbox
-      const checked = d3.select(this).property("checked");
+    //Filter
+    d3.selectAll(".form-check").on("click", function handleClick(){
+      update(data);
     });
+    
+    function update_filter(new_data){
+      for(row in new_data)
+      {
+        $('#filter_area'+(row%3)).append(`<div class="form-check">
+          <label class="form-check-label" for="` + new_data[row]['risk group'] + `">
+          <input type="checkbox" class="form-check-input" id="` + new_data[row]['risk group'] + `" checked>` + new_data[row]['risk group'] + `</label>
+        </div>`);
+      }
+    }
+
+    
   </script>
 </body>
 
